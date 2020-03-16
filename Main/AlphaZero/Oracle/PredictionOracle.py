412b8a14d5bf047d04be9d6318401b998c72b04f
ikaroszhang96 <shuyuanz@kth.se>
2020-03-16 18:13:45 +0100

    keras

--- Main/AlphaZero/Oracle/PredictionOracle.py
+++ Main/AlphaZero/Oracle/PredictionOracle.py
@@ -108,6 +108,9 @@ def _runOptimizedGraphOracle(model, selfPlayPool):
 
             # obtain the corresponding input-output tensor
             tf.import_graph_def(trt_graph, name='')
+            
+            a = model.layers[0]
+            a.weights
 
             INPUT_TENSOR = sess.graph.get_tensor_by_name('InputLayer:0')
             POLICY_OUT = sess.graph.get_tensor_by_name('convexInputLayer:0')
