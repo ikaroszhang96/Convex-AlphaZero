6ddfda614d528c97b07138347384fda4507e9923
ikaroszhang96 <shuyuanz@kth.se>
2020-03-17 13:47:17 +0100

    keras

--- Main/AlphaZero/Oracle/PredictionOracle.py
+++ Main/AlphaZero/Oracle/PredictionOracle.py
@@ -71,7 +71,8 @@ NORMAL_MODEL = None
 
 def _predictWithNormalModel(states):
     grads = K.gradients(NORMAL_MODEL.output,NORMAL_MODEL.input[1])
-    print(grads)
+    func = K.function([NORMAL_MODEL.input[0],NORMAL_MODEL.input[1]],[NORMAL_MODEL.output,grads])
+    print(func)
     return NORMAL_MODEL.predict([states])
 
 
